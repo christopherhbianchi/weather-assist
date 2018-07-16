fdcd2c71e573fbe8e23cdefd56146a5e137a0ecf
christopherhbianchi <chrishbianchi@gmail.com>
2018-07-16 16:35:29 -0600

    Added testing for weathActions, as well as a test for the fetch of the Dark Sky API request for a single weather object based on longitude and latitude.

--- src/actions/test/weatherActions.test.js
+++ src/actions/test/weatherActions.test.js
@@ -45,40 +45,27 @@ describe('weatherActions and weatherAction Creators', () => {
       });//it
     });//describe
 
-
-
   });//weatherActions
+
+
 
   //************************ Action Creator Fetch Requests **********************
   describe('weatherAction creator fetch requests', () => {
 
-    //mocking the fetch calls all at once.
-    window.fetch = jest.fn()
-      .mockImplementationOnce(() => {
-        status: 200,
-        json: () => new Promise( (resolve, reject) => {
-          resolve({
-            weather: { name:'Los Angeles', currently:{} }
-          })//resolve
-        })//json
-      }).mockImplementationOnce( () => {
-        return { status: 500 }
-      });
-
     const losAngelesUrl = 'https://api.darksky.net/forecast/33b9d25191f4ef8de9ee678ec256fcb4/37.8267,-122.4233';
-    const baseUrl = 'https://api.darksky.net/forecast/33b9d25191f4ef8de9ee678ec256fcb4/';
 
     describe('loadWeather fetch', () => {
       it('should return a weather object for Los Angeles', () => {
+        let losAngeles = {};
+        fetch(losAngelesUrl)
+          .then( async response => {
+            losAngeles = await response.json();
+          });//then
 
-      })
+          expect(losAngeles.timezone).toEqual('America/Los_Angeles');
+      });//it
     });//describe
-
-
-
-
-
 
   });//weatherAction Creators
 
-});//describe
+});//outermost describe
