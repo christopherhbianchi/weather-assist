5429cacca4cb86d912dcafbef1234c1b2f739cdc
christopherhbianchi <chrishbianchi@gmail.com>
2018-07-16 22:16:28 -0600

    Updated eslintrc file, and made changes throughout project to match desired eslint rules. Specified PropTypes in all components.

--- src/resources/CityCoordinates.js
+++ src/resources/CityCoordinates.js
@@ -1,24 +1,24 @@
 module.exports = {
 
   "LasVegas": {
-      "name": "Las Vegas",
-      "latitude": "36.1146",
-      "longitude": "-115.1728"
+    "name": "Las Vegas",
+    "latitude": "36.1146",
+    "longitude": "-115.1728"
   },
   "NewYork": {
-      "name": "New York",
-      "latitude": "40.7128",
-      "longitude": "-74.0060"
+    "name": "New York",
+    "latitude": "40.7128",
+    "longitude": "-74.0060"
   },
   "Miami": {
-      "name": "Miami",
-      "latitude": "25.7617",
-      "longitude": "-80.1918"
+    "name": "Miami",
+    "latitude": "25.7617",
+    "longitude": "-80.1918"
   },
   "LosAngeles": {
-      "name": "Los Angeles",
-      "latitude": "37.8267",
-      "longitude": "-122.4233"
+    "name": "Los Angeles",
+    "latitude": "37.8267",
+    "longitude": "-122.4233"
   }
 
-}
+};
